c301f025974cf194164eda72974f01b232450789
Joao v <filipimjoa@gmail.com>
2024-01-05 11:04:17 -0300

    Commit de teste para tirar dúvidas

---
+++ type.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('#tell').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '00000-000'
+    });
+})
