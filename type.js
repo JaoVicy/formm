4e01ff61f887cf71338a5533eececb977822d858
Joao v <filipimjoa@gmail.com>
2024-01-08 16:55:42 -0300

    Finalização do projeto

--- type.js
+++ type.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $('#tell').mask('(00) 00000-0000', {
         placeholder: '(00) 00000-0000'
     });
@@ -7,4 +6,42 @@ $(document).ready(function(){
     $('#cep').mask('00000-000', {
         placeholder: '00000-000'
     });
-})
+
+    $('#cpf').mask('000.000.000-00',{
+        placeholder: '000.000.000-00'
+    });
+
+
+    $('#form').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            tell: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            location: {
+                required: true
+            },
+            cep: {
+                required: true
+            },   
+        },
+        messages: {
+            name: 'Por favor insira seu nome.',
+            email: 'Por favor insira seu email.',
+            tell: 'Por favor insira seu número de celular.',
+            cpf: 'Por favor insira seu CPF.',
+            location: 'Por favor insira seu endereço.',
+            cep: 'Por favor insira o CEP de sua cidade.',
+        },
+        
+    });
+});
